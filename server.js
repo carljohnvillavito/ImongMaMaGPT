ea7d9598cc80f8ad4b6a280efb50bd1cc0617bfe
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-10-13 23:56:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'ImongMaMa')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'ImongMaMa', 'index.html')); // Change to chat.html if you have specific content for chat
-});
-
 // Serve index.html for the /chat route as well
 app.get('/chat', (req, res) => {
     res.sendFile(path.join(__dirname, 'ImongMaMa', 'index.html')); // Change to chat.html if you have specific content for chat
